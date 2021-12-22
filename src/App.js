443445e509711abc87ff594b81138893af5a6fb0
Mehul Shekhar Mukherjee <mehulshekharmukherjee@Mehuls-MacBook-Pro.local>
2021-12-22 15:15:07 -0500

    portfolio UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useContext } from 'react'
+import { ThemeContext } from './contexts/theme'
+import Header from './components/Header/Header'
+import About from './components/About/About'
+import Projects from './components/Projects/Projects'
+import Skills from './components/Skills/Skills'
+import ScrollToTop from './components/ScrollToTop/ScrollToTop'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [{ themeName }] = useContext(ThemeContext)
+
   return (
-    <div className="App">
-      Hello
+    <div id='top' className={`${themeName} app`}>
+      <Header />
+
+      <main>
+        <About />
+        <Projects />
+        <Skills />
+        <Contact />
+      </main>
+
+      <ScrollToTop />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
